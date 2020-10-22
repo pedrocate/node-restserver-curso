cac413fd906550dc7994e7244ac6dc927e8c9803
PedroCate <pedro.cate.7@gmail.com>
2020-10-22 16:15:59 +0200

    Quitamos el verifica token para crear un usuario con contraseña, que no me acuerdo jeje

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -34,7 +34,7 @@ app.get('/usuario', verificaToken, (req, res) => {
 
 });
 
-app.post('/usuario', [verificaToken, verificaAdminRole], function(req, res) {
+app.post('/usuario', function(req, res) {
 
     let body = req.body;
 
